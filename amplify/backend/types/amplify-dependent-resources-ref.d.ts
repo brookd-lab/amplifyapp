4e06a8392e4e48eb06eecaca0f6bc43c63314749
brookd-lab <54964060+brookd-lab@users.noreply.github.com>
2023-01-22 14:50:03 -0800

    Added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ampreactgraphql26af7473": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
